a2e7729fb70d80a210a98a110d003a71aba9f498
Dave <dave.dewire@gmail.com>
2016-11-16 05:27:55 +0000

    completed

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var parser = require('ua-parser-js');
+var r={};
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    r.ip=req.headers['x-forwarded-for'];
+    var templang=req.headers['accept-language']
+
+    var lang=templang.split(",");
+    console.log(lang[0]);
+    r.language=lang[0];
+    var agent=req.headers['user-agent'];
+    var ua = parser(req.headers['user-agent']);
+    r.OS=ua.os.name + ' ' + ua.os.version;
+    res.send(r);
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.listen(8080);
 
 
